2bc38cf3c5bee36681cc9e90d6b161f81b94c2a7
name <subbusubhash1592@gmail.com>
2023-02-28 13:43:00 +0530

    second commit

--- app/serializers.py
+++ app/serializers.py
@@ -6,6 +6,6 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__' #this allows us to serialize the field which we want
-        
+        #testing
         # exclude = ['id','name']  #here i can exclude id and name remaining fields will be serialized
         # fields = '__all__'  #here i can serialize all the fields which are available in my Student model
